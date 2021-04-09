557204a6d5473f727388d7c92d242c7defb3c036
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-04-09 09:00:43 -0400

    made the ids in the 3 tables auto increment and created a seeds file to pupulate the tables as recommended

--- schema.sql
+++ schema.sql
@@ -5,14 +5,14 @@ CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR (30),
     PRIMARY KEY (id)
 
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR (30),
     salary DECIMAL (10,4),
     department_id INT NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT NOT NULL,
